8bbd9dc4d7c8b8c237c35ee231349319c38c2b50
suma1892 <sumaalbaroh1892@gmail.com>
2025-05-01 16:05:18 +0700

    fix: change invalid address

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/private/', '/admin/'],
     },
-    sitemap: 'https://alimrugi.id/sitemap.xml',
+    sitemap: 'https://alimtech.id/sitemap.xml',
   };
 } 
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://alimrugi.id';
+  const baseUrl = 'https://alimtech.id';
   
   return [
     {
